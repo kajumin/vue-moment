0c490d8d89cafe552f19aa0e7d821c7546f6a851
kajumin <404107262@qq.com>
2021-05-30 23:04:16 +0800

    新增gzip压缩优化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 const isProd = process.env.NODE_ENV === 'production'
 console.log(isProd)
 
@@ -25,6 +27,16 @@ const config = {
     //     return arg
     //   })
     // }
+    if (isProd) {
+      // 配置webpack 压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css$/,
+          // 超过4kb压缩
+          threshold: 4096
+        })
+      )
+    }
   }
 }
 module.exports = config
